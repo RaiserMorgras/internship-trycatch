08b5225fd3201b827d85fe961609439abc95b048
Arseniy Gromovoy <arseniygromovoy@gmail.com>
2020-02-07 15:17:31 +0200

    Method rewritten using throws. Tests adjusted for new method.

--- src/tryCatchTest/StringIntegerDivider.java
+++ src/tryCatchTest/StringIntegerDivider.java
@@ -1,16 +1,7 @@
 package tryCatchTest;
 
 public class StringIntegerDivider {
-    public static String getMessageOnRoundDivision(String x, String y) {
-        try {
-            int result = Integer.parseInt(x) / Integer.parseInt(y);
-            return Integer.toString(result);
-        }
-        catch (ArithmeticException e) {
-            return e.toString();
-        }
-        catch (NumberFormatException e) {
-            return e.getClass().getName();
-        }
+    public static int divideIntegersInStrings(String x, String y) throws ArithmeticException, NumberFormatException{
+        return Integer.parseInt(x) / Integer.parseInt(y);
     }
 }
--- src/tryCatchTest/TestCases.java
+++ src/tryCatchTest/TestCases.java
@@ -1,48 +1,49 @@
 package tryCatchTest;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestCases {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void testCase1() {
         String x = "10";
         String y = "3";
-        String expected = "3";
-        String actual = StringIntegerDivider.getMessageOnRoundDivision(x, y);
+        int expected = 3;
+        int actual = StringIntegerDivider.divideIntegersInStrings(x, y);
 
         assertEquals(expected, actual);
     }
 
-    @Test
+    @Test(expected = java.lang.NumberFormatException.class)
     public void testCase2() {
         String x = "10";
         String y = "Hello";
-        String expected = "java.lang.NumberFormatException";
-        String actual = StringIntegerDivider.getMessageOnRoundDivision(x, y);
 
-        assertEquals(expected, actual);
+        StringIntegerDivider.divideIntegersInStrings(x, y);
     }
 
     @Test
     public void testCase3() {
+        thrown.expect(java.lang.ArithmeticException.class);
+        thrown.expectMessage("/ by zero");
+
         String x = "10";
         String y = "0";
-        String expected = "java.lang.ArithmeticException: / by zero";
-        String actual = StringIntegerDivider.getMessageOnRoundDivision(x, y);
 
-        assertEquals(expected, actual);
+        StringIntegerDivider.divideIntegersInStrings(x, y);
     }
 
-    @Test
+    @Test(expected = java.lang.NumberFormatException.class)
     public void testCase4() {
         String x = "23.323";
         String y = "0";
-        String expected = "java.lang.NumberFormatException";
-        String actual = StringIntegerDivider.getMessageOnRoundDivision(x, y);
 
-        assertEquals(expected, actual);
+        StringIntegerDivider.divideIntegersInStrings(x, y);
     }
 }
